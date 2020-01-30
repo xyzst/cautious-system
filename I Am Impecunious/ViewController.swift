0d342012cfc4c5c868aaf5e5bc8d6bfdb531c483
Darren Rambaud <xyzst@users.noreply.github.com>
2020-01-29 19:24:17 -0600

    Project Initialization
    Basic project structure provided by Xcode
    
    Related to: none

---
+++ I Am Impecunious/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Impecunious
+//
+//  Created by Darren Rambaud on 1/29/20.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am ImpecuniousTests/I_Am_ImpecuniousTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_Am_ImpecuniousTests.swift
+//  I Am ImpecuniousTests
+//
+//  Created by Darren Rambaud on 1/29/20.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import XCTest
+@testable import I_Am_Impecunious
+
+class I_Am_ImpecuniousTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
